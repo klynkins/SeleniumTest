ec482c570bf9ec98cd4aa2d8516acf8e17c08daf
Katelynn Campbell <klyncamp23@gmail.com>
2018-06-27 10:26:03 -0400

    Test to open browser

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService("/Users/katelynncampbell/Documents/SeleniumTest/bin/Debug/netcoreapp2.1");
+            IWebDriver driver = new FirefoxDriver(service);
+
+            driver.Url = "https://www.yahoo.com/";
+        }
+    }
+}
